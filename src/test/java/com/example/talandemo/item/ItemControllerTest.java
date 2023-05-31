160196b5433cb5d1e7ddd87d5956e8ddb4ba7469
Krystian Laskowski <krystian.laskowski.1@gmail.com>
2023-05-31 18:39:35 +0200

    Improved test coverage

--- src/test/java/com/example/talandemo/item/ItemControllerTest.java
+++ src/test/java/com/example/talandemo/item/ItemControllerTest.java
@@ -9,13 +9,22 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebMvcTest(ItemController.class)
 public class ItemControllerTest {
@@ -47,14 +56,101 @@ public class ItemControllerTest {
 
     when(itemService.createItem(any(Item.class))).thenReturn(item);
 
-    mockMvc.perform(MockMvcRequestBuilders.post("/items")
+    mockMvc.perform(post("/items")
             .contentType(MediaType.APPLICATION_JSON)
             .content(
                 "{\"name\":\"Sample Item\",\"price\":10.99,\"description\":\"Sample description\"}"))
-        .andExpect(MockMvcResultMatchers.status().isCreated())
-        .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
-        .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Sample Item"))
-        .andExpect(MockMvcResultMatchers.jsonPath("$.price").value(10.99))
-        .andExpect(MockMvcResultMatchers.jsonPath("$.description").value("Sample description"));
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id").value(1))
+        .andExpect(jsonPath("$.name").value("Sample Item"))
+        .andExpect(jsonPath("$.price").value(10.99))
+        .andExpect(jsonPath("$.description").value("Sample description"));
   }
+
+  @Test
+  void updateItem_ReturnsUpdatedItem() throws Exception {
+    // Arrange
+    Long itemId = 1L;
+    Item updatedItem = new Item();
+    updatedItem.setName("Updated Item");
+    updatedItem.setDescription("This is the updated item");
+
+    when(itemService.updateItem(eq(itemId), any(Item.class))).thenReturn(updatedItem);
+
+    // Act & Assert
+    mockMvc.perform(put("/items/{id}", itemId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"name\":\"Updated Item\",\"description\":\"This is the updated item\"}"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.name").value("Updated Item"))
+        .andExpect(jsonPath("$.description").value("This is the updated item"));
+
+    verify(itemService, times(1)).updateItem(eq(itemId), any(Item.class));
+  }
+
+  @Test
+  void deleteItem_ReturnsNoContent() throws Exception {
+    // Arrange
+    Long itemId = 1L;
+
+    when(itemService.deleteItem(itemId)).thenReturn(true);
+
+    // Act & Assert
+    mockMvc.perform(delete("/items/{id}", itemId))
+        .andExpect(status().isNoContent());
+
+    verify(itemService, times(1)).deleteItem(itemId);
+  }
+
+  @Test
+  void getItemById_ReturnsItem() throws Exception {
+    // Arrange
+    Long itemId = 1L;
+    Item item = new Item();
+    item.setId(itemId);
+    item.setName("Test Item");
+    item.setDescription("This is a test item");
+
+    when(itemService.getItemById(itemId)).thenReturn(item);
+
+    // Act & Assert
+    mockMvc.perform(get("/items/{id}", itemId))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.id").value(itemId))
+        .andExpect(jsonPath("$.name").value("Test Item"))
+        .andExpect(jsonPath("$.description").value("This is a test item"));
+
+    verify(itemService, times(1)).getItemById(itemId);
+  }
+
+  @Test
+  void getAllItems_ReturnsAllItems() throws Exception {
+    // Arrange
+    List<Item> itemList = new ArrayList<>();
+    Item item1 = new Item();
+    item1.setId(1L);
+    item1.setName("Item 1");
+    item1.setDescription("This is item 1");
+    itemList.add(item1);
+    Item item2 = new Item();
+    item2.setId(2L);
+    item2.setName("Item 2");
+    item2.setDescription("This is item 2");
+    itemList.add(item2);
+
+    when(itemService.getAllItems()).thenReturn(itemList);
+
+    // Act & Assert
+    mockMvc.perform(get("/items"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$[0].id").value(1L))
+        .andExpect(jsonPath("$[0].name").value("Item 1"))
+        .andExpect(jsonPath("$[0].description").value("This is item 1"))
+        .andExpect(jsonPath("$[1].id").value(2L))
+        .andExpect(jsonPath("$[1].name").value("Item 2"))
+        .andExpect(jsonPath("$[1].description").value("This is item 2"));
+
+    verify(itemService, times(1)).getAllItems();
+  }
+
 }
